8c806f743bef2580bba85abfe3f153b1a5cab337
Faran <faran.7j99@gmail.com>
2025-02-18 06:49:09 +0330

    add navigation to the featured button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom"
+
 // eslint-disable-next-line react/prop-types
-export default function Button({text}) {
-  return (
-        <button className={`w-full bg-secondary text-white rounded-[5px] cursor-pointer h-13 font-bold`}>
+export default function Button({ text }) {
+    const navigate = useNavigate();
+
+    return (
+        <button className={`w-full bg-secondary text-white rounded-[5px] cursor-pointer h-13 font-bold`} onClick={() => navigate('vans')}>
             {text}
         </button>
-  )
+    )
 }
