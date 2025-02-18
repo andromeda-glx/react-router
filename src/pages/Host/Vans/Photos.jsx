1f0d8d88d30197380faa25ae9f02f79b331f5d30
Faran <faran.7j99@gmail.com>
2025-02-18 07:24:38 +0330

    fix host vans photos section's

--- src/pages/Host/Vans/Photos.jsx
+++ src/pages/Host/Vans/Photos.jsx
@@ -4,20 +4,22 @@ import Spinner from "../../../components/Spinner";
 
 export default function Photos() {
     const { id } = useParams();
-    const { data, isLoading, error } = useFetchData(`/api/vans/${id}`);
+    const { data, isLoading, error } = useFetchData(`/api/host/vans/${id}`);
+
+    const van = data?.vans[0];
 
     return (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 overflow-y-auto p-1">
-            {!error ? isLoading ? <Spinner /> : data?.vans &&
+            {!error ? isLoading ? <Spinner /> : van &&
                 <>
                     <div className="w-[100%]">
-                        <img className="w-[100%] rounded-lg" src={data.vans.imageUrl} alt={data.vans.name} />
+                        <img className="w-[100%] rounded-lg" src={van.imageUrl} alt={van.name} />
                     </div>
                     <div className="w-[100%]">
-                        <img className="w-[100%] rounded-lg" src={data.vans.imageUrl} alt={data.vans.name} />
+                        <img className="w-[100%] rounded-lg" src={van.imageUrl} alt={van.name} />
                     </div>
                     <div className="w-[100%]">
-                        <img className="w-[100%] rounded-lg" src={data.vans.imageUrl} alt={data.vans.name} />
+                        <img className="w-[100%] rounded-lg" src={van.imageUrl} alt={van.name} />
                     </div>
                 </>
                 : <p>Network Error</p>}
